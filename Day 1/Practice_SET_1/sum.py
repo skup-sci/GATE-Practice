7102cfba2f193e91cef404eca04704c41b5aed95
Shashank Upadhyay <upadhyayshashank048@gmail.com>
2024-06-22 01:32:25 +0530

    uploaded GFG Assignments

---
+++ Day 1/Practice_SET_1/sum.py
@@ -0,0 +1,5 @@
+numbers = input("Enter four numbers.")
+numbers = numbers.split()  # split the input into a list of strings
+#find the sum of these numbers
+print("The sum of these numbers is", sum(int(numbers)))
+ 
